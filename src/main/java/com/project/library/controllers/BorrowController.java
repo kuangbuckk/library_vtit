ef81b975beba480c3e84ac649d82c512ee975ba6
kuangbuckk <kuangbuckk159@gmail.com>
2025-02-20 18:02:55 +0700

    refactor(Book): add paging request for retrieve all in controller

--- src/main/java/com/project/library/controllers/BorrowController.java
+++ src/main/java/com/project/library/controllers/BorrowController.java
@@ -2,10 +2,13 @@ package com.project.library.controllers;
 
 import com.project.library.dtos.BorrowDTO;
 import com.project.library.entities.Borrow;
+import com.project.library.responses.BorrowPageResponse;
+import com.project.library.responses.BorrowResponse;
 import com.project.library.responses.GenericResponse;
 import com.project.library.services.interfaces.IBorrowService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -22,19 +25,22 @@ public class BorrowController {
     private final IBorrowService borrowService;
 
     @GetMapping("/")
-    public ResponseEntity<GenericResponse<List<Borrow>>> getBorrows() {
-        List<Borrow> borrowList = borrowService.getAllBorrows();
-        return ResponseEntity.ok(GenericResponse.success(borrowList));
+    public ResponseEntity<GenericResponse> getBorrows(
+            @RequestParam("page_number") int pageNumber,
+            @RequestParam("size") int size
+    ) {
+        BorrowPageResponse borrowPageResponse = borrowService.getAllBorrows(PageRequest.of(pageNumber, size));
+        return ResponseEntity.ok(GenericResponse.success(borrowPageResponse));
     }
 
     @GetMapping("/{code}")
-    public ResponseEntity<GenericResponse<Borrow>> getBorrow(@PathVariable String code) {
-        Borrow existingBorrow = borrowService.getBorrowByCode(UUID.fromString(code));
-        return ResponseEntity.ok(GenericResponse.success(existingBorrow));
+    public ResponseEntity<GenericResponse> getBorrow(@PathVariable String code) {
+        BorrowResponse borrowResponse = borrowService.getBorrowByCode(UUID.fromString(code));
+        return ResponseEntity.ok(GenericResponse.success(borrowResponse));
     }
 
     @PostMapping("/")
-    public ResponseEntity<GenericResponse<Borrow>> addBorrow(
+    public ResponseEntity<GenericResponse> addBorrow(
             @RequestBody @Valid BorrowDTO borrowDTO,
             BindingResult result) {
         if (result.hasErrors()) {
@@ -44,10 +50,29 @@ public class BorrowController {
                     .toList();
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(GenericResponse.error(errors.toString()));
         }
-        Borrow newBorrow = borrowService.addBorrow(borrowDTO);
-        return ResponseEntity.ok(GenericResponse.success(newBorrow));
+        BorrowResponse borrowResponse = borrowService.addBorrow(borrowDTO);
+        return ResponseEntity.ok(GenericResponse.success(borrowResponse));
     }
 
-    //TODO: add update and delete
+    @PutMapping("/{code}")
+    public ResponseEntity<GenericResponse> updateBorrow(
+            @RequestBody @Valid BorrowDTO borrowDTO,
+            @PathVariable UUID code,
+            BindingResult result) {
+        if (result.hasErrors()) {
+            List<String> errors = result.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .toList();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(GenericResponse.error(errors.toString()));
+        }
+        BorrowResponse updateBorrowResponse = borrowService.updateBorrow(borrowDTO, code);
+        return ResponseEntity.ok(GenericResponse.success(updateBorrowResponse));
+    }
 
+    @DeleteMapping("/{code}")
+    public ResponseEntity<GenericResponse> deleteBorrow(@PathVariable String code) {
+        borrowService.deleteBorrow(UUID.fromString(code));
+        return ResponseEntity.ok(GenericResponse.success(code));
+    }
 }
