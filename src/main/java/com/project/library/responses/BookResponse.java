6b4272f53e4b938053307bd456f6a76f08ebd379
kuangbuckk <kuangbuckk159@gmail.com>
2025-02-22 10:54:13 +0700

    refactor(Book): refactor book's category response

--- src/main/java/com/project/library/responses/BookResponse.java
+++ src/main/java/com/project/library/responses/BookResponse.java
@@ -3,15 +3,8 @@ package com.project.library.responses;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.project.library.entities.Book;
 import com.project.library.entities.Category;
-import com.project.library.entities.Post;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.*;
-import org.hibernate.validator.cfg.defs.UUIDDef;
-import org.springframework.data.domain.Page;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -26,25 +19,24 @@ public class BookResponse {
     private String title;
     private int amount;
 
-    @JsonProperty("category_codes")
-    private List<UUID> categoryCodes;
+    @JsonProperty("category_names")
+    private List<String> categoryNames;
 
 //    @JsonProperty("posts_codes")
 //    private List<UUID> postsCodes;
 
     public static BookResponse fromBook(Book book) {
-        List<UUID> categoryCodes = book.getCategories().stream().map(Category::getCode).toList();
-        List<UUID> postCodes = book.getPosts().stream().map(Post::getCode).toList();
+        List<String> categoryNames = book.getCategories().stream().map(Category::getCategoryName).toList();
+//        List<UUID> postCodes = book.getPosts().stream().map(Post::getCode).toList();
 
-        BookResponse bookResponse = BookResponse.builder()
+        return BookResponse.builder()
                 .code(book.getCode())
                 .author(book.getAuthor())
                 .title(book.getTitle())
                 .amount(book.getAmount())
-                .categoryCodes(categoryCodes)
+                .categoryNames(categoryNames)
 //                .postsCodes(postCodes)
                 .build();
-        return bookResponse;
     }
 
 }
