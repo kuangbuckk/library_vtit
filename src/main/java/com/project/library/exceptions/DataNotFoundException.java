d305f4f6ff5815aab4e5ebff56fc8ddb5b6cd831
kuangbuckk <kuangbuckk159@gmail.com>
2025-02-27 17:41:47 +0700

    exception(): add new type of data out of bound exception and refactor DataNotFound exception with id

--- src/main/java/com/project/library/exceptions/DataNotFoundException.java
+++ src/main/java/com/project/library/exceptions/DataNotFoundException.java
@@ -1,7 +1,18 @@
 package com.project.library.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Getter
 public class DataNotFoundException extends RuntimeException {
-    public DataNotFoundException(String message) {
+    public UUID code;
+
+    public DataNotFoundException(String message, UUID code) {
         super(message);
+        this.code = code;
     }
 }
---
+++ src/main/java/com/project/library/exceptions/DataOutOfBoundException.java
@@ -0,0 +1,7 @@
+package com.project.library.exceptions;
+
+public class DataOutOfBoundException extends RuntimeException {
+    public DataOutOfBoundException(String message) {
+        super(message);
+    }
+}
