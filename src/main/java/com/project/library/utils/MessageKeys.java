b4724f64a0ae0824d8a366cee0b0456428a32ccf
kuangbuckk <kuangbuckk159@gmail.com>
2025-03-01 10:47:44 +0700

    feat(i18n): add illegal input arguments

--- src/main/java/com/project/library/utils/MessageKeys.java
+++ src/main/java/com/project/library/utils/MessageKeys.java
@@ -75,4 +75,6 @@ public class MessageKeys {
     public static final String WRONG_PHONE_PASSWORD = "user.login.wrong_phone_password";
     public static final String ROLE_DOES_NOT_EXISTS = "user.login.role_not_exist";
 
+    public static final String ILLEGAL_INPUT_ARGUMENT = "illegal.input.argument";
+
 }
