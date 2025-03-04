30ec60d896b5849bedc572f350563ed687fc7b03
kuangbuckk <kuangbuckk159@gmail.com>
2025-03-04 14:52:22 +0700

    refactor(RoleGroupRepository): refactor query for functions by a role

--- src/main/java/com/project/library/repositories/RoleGroupRepository.java
+++ src/main/java/com/project/library/repositories/RoleGroupRepository.java
@@ -1,11 +1,19 @@
 package com.project.library.repositories;
 
+import com.project.library.entities.Function;
 import com.project.library.entities.RoleGroup;
+import jakarta.persistence.Entity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface RoleGroupRepository extends JpaRepository<RoleGroup, UUID> {
+//    @EntityGraph(attributePaths = {"functions"})
+    @Query("SELECT rg.functions FROM RoleGroup rg WHERE rg.roleGroupName = :roleGroupName")
+    List<Function> findFunctionsByRoleGroupName(String roleGroupName);
 }
