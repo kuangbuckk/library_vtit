38f190feaf028369a94a1523051c3ef59d4b8b9b
kuangbuckk <kuangbuckk159@gmail.com>
2025-03-18 16:36:57 +0700

    refactor(Scheduling): add connection and read timeout for RestTemplate

--- src/main/java/com/project/library/configurations/RestTemplateConfig.java
+++ src/main/java/com/project/library/configurations/RestTemplateConfig.java
@@ -1,13 +1,26 @@
 package com.project.library.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class RestTemplateConfig {
+    @Value("${rest-template.connection-time-out}")
+    private int connectionTimeOut;
+
+    @Value("${rest-template.read-time-out}")
+    private int readTimeOut;
+
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofMillis(connectionTimeOut))
+                .setReadTimeout(Duration.ofMillis(readTimeOut))
+                .build();
     }
 }
