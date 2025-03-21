2dcca4141e36810f0d88e95da3db25f05dad7998
kuangbuckk <kuangbuckk159@gmail.com>
2025-03-21 19:37:07 +0700

    feat(i18n): add keys and messages

--- src/main/java/com/project/library/utils/MessageKeys.java
+++ src/main/java/com/project/library/utils/MessageKeys.java
@@ -15,37 +15,46 @@ public class MessageKeys {
     public static final String DELETE_CATEGORY_FAILED = "category.delete.error";
     public static final String CATEGORY_NOT_FOUND = "category.find.error";
 
+    public static final String GET_BOOK_SUCCESSFULLY = "book.view.success";
     public static final String INSERT_BOOK_SUCCESSFULLY = "book.create.success";
     public static final String UPDATE_BOOK_SUCCESSFULLY = "book.update.success";
     public static final String DELETE_BOOK_SUCCESSFULLY = "book.delete.success";
+    public static final String DESTROY_BOOK_SUCCESSFULLY = "book.delete.success";
     public static final String INSERT_BOOK_FAILED = "book.create.error";
     public static final String UPDATE_BOOK_FAILED = "book.update.error";
     public static final String DELETE_BOOK_FAILED = "book.delete.error";
+    public static final String DESTROY_BOOK_FAILED = "book.destroy.error";
     public static final String BOOK_NOT_FOUND = "book.find.error";
     public static final String BOOK_OUT_OF_STOCK = "book.out_of_stock";
 
     public static final String INSERT_POST_SUCCESSFULLY = "post.create.success";
     public static final String UPDATE_POST_SUCCESSFULLY = "post.update.success";
     public static final String DELETE_POST_SUCCESSFULLY = "post.delete.success";
+    public static final String DESTROY_POST_SUCCESSFULLY = "post.destroy.success";
     public static final String INSERT_POST_FAILED = "post.create.error";
     public static final String UPDATE_POST_FAILED = "post.update.error";
     public static final String DELETE_POST_FAILED = "post.delete.error";
+    public static final String DESTROY_POST_FAILED = "post.destroy.error";
     public static final String POST_NOT_FOUND = "post.find.error";
 
     public static final String INSERT_COMMENT_SUCCESSFULLY = "comment.create.success";
     public static final String UPDATE_COMMENT_SUCCESSFULLY = "comment.update.success";
     public static final String DELETE_COMMENT_SUCCESSFULLY = "comment.delete.success";
+    public static final String DESTROY_COMMENT_SUCCESSFULLY = "comment.destroy.success";
     public static final String INSERT_COMMENT_FAILED = "comment.create.error";
     public static final String UPDATE_COMMENT_FAILED = "comment.update.error";
     public static final String DELETE_COMMENT_FAILED = "comment.delete.error";
+    public static final String DESTROY_COMMENT_FAILED = "comment.delete.error";
     public static final String COMMENT_NOT_FOUND = "comment.find.error";
 
     public static final String INSERT_USER_SUCCESSFULLY = "user.create.success";
     public static final String UPDATE_USER_SUCCESSFULLY = "user.update.success";
     public static final String DELETE_USER_SUCCESSFULLY = "user.delete.success";
+    public static final String DESTROY_USER_SUCCESSFULLY = "user.delete.success";
     public static final String INSERT_USER_FAILED = "user.create.error";
     public static final String UPDATE_USER_FAILED = "user.update.error";
     public static final String DELETE_USER_FAILED = "user.delete.error";
+    public static final String DESTROY_USER_FAILED = "user.delete.error";
     public static final String USER_NOT_FOUND = "user.find.error";
 
     public static final String INSERT_ROLE_GROUP_SUCCESSFULLY = "role_group.create.success";
@@ -67,9 +76,11 @@ public class MessageKeys {
     public static final String INSERT_BORROW_SUCCESSFULLY = "borrow.create.success";
     public static final String UPDATE_BORROW_SUCCESSFULLY = "borrow.update.success";
     public static final String DELETE_BORROW_SUCCESSFULLY = "borrow.delete.success";
+    public static final String DESTROY_BORROW_SUCCESSFULLY = "borrow.destroy.success";
     public static final String INSERT_BORROW_FAILED = "borrow.create.error";
     public static final String UPDATE_BORROW_FAILED = "borrow.update.error";
     public static final String DELETE_BORROW_FAILED = "borrow.delete.error";
+    public static final String DESTROY_BORROW_FAILED = "borrow.destroy.error";
     public static final String BORROW_NOT_FOUND = "borrow.find.error";
 
     public static final String WRONG_PHONE_PASSWORD = "user.login.wrong_phone_password";
